1225c6ad3d835c8d7dc39df7dd87c438247cf549
lisakutemeier <abced1@web.de>
2025-03-05 18:19:53 +0100

    added more changes from laura's branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Impressum from "./pages/Impressum";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
     return (
         <Router>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/impressum" element={<Impressum />} />
-            </Routes>
+            <div id="dynamic-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/impressum" element={<Impressum />} />
+                </Routes>
+            </div>
         </Router>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
