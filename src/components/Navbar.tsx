c78425b3eb2e8c3d18d41bf51af0a6186bc2327e
laura194 <voss.2000@gmx.de>
2025-03-05 15:17:51 +0100

    restructure navbar with bootstrap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,50 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="nav-container">
-                <Link to="/" className="nav-logo">
-                    MyWebshop
-                </Link>
-                <ul className="nav-menu">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/shop" className="nav-link">Shop</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-link">About</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/impressum" className="nav-link">Impressum</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          MyWebshop
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/shop" className="nav-link">
+                Shop
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/impressum" className="nav-link">
+                Impressum
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
