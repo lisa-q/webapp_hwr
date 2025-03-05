f72ec9ebc90614c45b3402854373b663dcf7c852
laura194 <voss.2000@gmx.de>
2025-03-05 15:59:07 +0100

    app.css eingebunden (damit alle dynamischen seiten das selbe grundstyling haben

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Impressum from "./pages/Impressum";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/impressum" element={<Impressum />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <div id="dynamic-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/impressum" element={<Impressum />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
