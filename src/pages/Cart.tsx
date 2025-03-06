5443c8b57e32dc7cda27ac0a288ac7c084e53f81
laura194 <voss.2000@gmx.de>
2025-03-06 12:57:54 +0100

    Warenkorb seite weiter angepasst -> delete item from cart (bekommt den aktuellen warenkorb nicht mehr übergeben, sondern zieht den selber aus der datenbank.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,36 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CartItem } from "../types/cartItem";
 import CartItemComponent from "../components/CartItemComponent";
 import "./Cart.css";
+import CartFirebaseService from "../services/CartFirebaseService";
 
-interface CartProps {
-  cartItems: CartItem[];
-}
+const Cart: React.FC = () => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-const Cart: React.FC<CartProps> = ({ cartItems }) => {
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      const items = await CartFirebaseService.getCurrentCart();
+      setCartItems(items);
+    };
+    fetchCartItems();
+  }, []);
+
+  const handleDeleteItem = async (id: string) => {
+    try {
+      await CartFirebaseService.removeFromCart(id);
+      setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+      console.log("Item gelöscht:", id);
+    } catch (error) {
+      console.error("Fehler beim Löschen des Items:", error);
+    }
+  };
+
   return (
-    <div>
+    <div className="cart-container">
       <h1>Warenkorb</h1>
       {cartItems.length === 0 ? (
         <p>Dein Warenkorb ist leer.</p>
       ) : (
-        <div>
-          <ul className="list-cartItems">
-            {cartItems.map((item, index) => (
-              <CartItemComponent cartItem={item} key={index} />
-            ))}
-          </ul>
-          <div>
+        <>
+          <div className="cart-items-wrapper">
+            <ul className="list-cartItems">
+              {cartItems.map((item) => (
+                <CartItemComponent
+                  key={item.id}
+                  cartItem={item}
+                  onDelete={handleDeleteItem}
+                />
+              ))}
+            </ul>
+          </div>
+          <div className="cart-total">
             <h2>
               Gesamt:{" "}
-              {cartItems.reduce(
-                (total, item) => total + item.quantity * item.price,
-                0
-              )}{" "}
+              {cartItems
+                .reduce((total, item) => total + item.quantity * item.price, 0)
+                .toFixed(2)}{" "}
               €
             </h2>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
