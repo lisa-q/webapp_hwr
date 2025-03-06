28253c63389957786cc8cf44343d84b9c501c1cd
laura194 <voss.2000@gmx.de>
2025-03-06 18:39:25 +0100

    addToCart angepasst. item bereits existiert, dann anzahl um eins erhöhen updateQuantityCartItemById hinzugefügt

--- src/services/CartFirebaseService.ts
+++ src/services/CartFirebaseService.ts
@@ -58,14 +58,46 @@ class CartFirebaseService {
     static async addToCart(cartItem: CartItem): Promise<void> {
         const deviceId = this.getDeviceId();
         const dataRef = ref(db, `carts/${deviceId}/${cartItem.id}`);
-        await set(dataRef, cartItem);
+        const snapshot = await get(dataRef);
+    
+        if (snapshot.exists()) {
+            const existingItem = snapshot.val() as CartItem;
+            const updatedQuantity = (existingItem.quantity ?? 1) + 1;
+            await set(dataRef, { ...existingItem, quantity: updatedQuantity });
+        } else {
+            await set(dataRef, { ...cartItem, quantity: 1 });
+        }
     }
+    
 
     static async removeFromCart(cartItemId: string): Promise<void> {
         const deviceId = this.getDeviceId();
         const dataRef = ref(db, `carts/${deviceId}/${cartItemId}`);
         await remove(dataRef);
     }
+
+    static async updateQuantityCartItemById(cartItemId: string, newQuantity: number): Promise<void> {
+        const deviceId = this.getDeviceId();
+        const itemRef = ref(db, `carts/${deviceId}/${cartItemId}`);
+    
+        if (newQuantity > 0) {
+            const snapshot = await get(itemRef);
+    
+            if (snapshot.exists()) {
+                const existingData = snapshot.val();
+    
+                // Bestehende Daten mit neuer Menge speichern
+                await set(itemRef, {
+                    ...existingData,
+                    quantity: newQuantity,
+                });
+            }
+        } else {
+            await remove(itemRef);
+        }
+    }
+    
+    
 
     static async placeOrder(orderDetails: {
         address: {
