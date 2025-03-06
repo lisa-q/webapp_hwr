9b0b25ec324dcce0fff94308dfa776dbbd4512c3
laura194 <voss.2000@gmx.de>
2025-03-06 12:56:44 +0100

    CartOverlay hinzugefügt (gehört zur Navbar) zeigt den aktuellen Warenkorb an.

---
+++ src/components/CartOverlay.tsx
@@ -0,0 +1,60 @@
+import "./CartOverlay.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartItem } from "../types/cartItem";
+import CartItemComponent from "./CartItemComponent";
+import CartFirebaseService from "../services/CartFirebaseService";
+
+const CartOverlay = ({ onClose }: { onClose: () => void }) => {
+  const navigate = useNavigate();
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      const items = await CartFirebaseService.getCurrentCart();
+      setCartItems(items);
+    };
+    fetchCartItems();
+  }, []);
+
+  const handleOpenCart = () => {
+    navigate("/cart", { state: { cartItems } });
+  };
+
+  const handleDeleteItem = async (id: string) => {
+    await CartFirebaseService.removeFromCart(id);
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+    console.log("Item gelöscht:", id);
+  };
+
+  return (
+    <div className="cart-overlay">
+      <div className="cart-content">
+        <button className="close-btn" onClick={onClose}>
+          &times;
+        </button>
+        <h2>Dein Warenkorb</h2>
+        <div className="cart-items-container">
+          {cartItems.length > 0 ? (
+            <ul className="cart-items">
+              {cartItems.map((item) => (
+                <CartItemComponent
+                  key={item.id}
+                  cartItem={item}
+                  onDelete={handleDeleteItem}
+                />
+              ))}
+            </ul>
+          ) : (
+            <p>Dein Warenkorb ist leer.</p>
+          )}
+        </div>
+        <button className="btn btn-outline-warning" onClick={handleOpenCart}>
+          Open Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartOverlay;
