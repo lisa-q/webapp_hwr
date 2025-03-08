42893776a7b2f87d72468336a7b0c05463cbe6fa
laura194 <voss.2000@gmx.de>
2025-03-08 14:50:57 +0100

    button-farbe angepasst. ich hatte das kontaktformular rausgenommen, weil wir es mit nichts verbunden haben. habe hier den satz mit dem formular also auch entfernt

--- src/pages/ThankPage.tsx
+++ src/pages/ThankPage.tsx
@@ -2,24 +2,21 @@ import { Link } from "react-router-dom";
 import "./ThankPage.css";
 
 const ThankPage = () => {
-    return (
-        <div className="container thank-container">
-            <h2>Vielen Dank für deine Bestellung! 🎉</h2>
-            <p>
-                Deine Bestellung wurde erfolgreich aufgegeben.
-                Falls du Fragen hast, stehen wir dir gerne über unser Kontaktformular zur Verfügung.
-            </p>
+  return (
+    <div className="container thank-container">
+      <h2>Vielen Dank für deine Bestellung! 🎉</h2>
+      <p>Deine Bestellung wurde erfolgreich aufgegeben.</p>
 
-            <div className="thank-buttons">
-                <Link to="/shop" className="btn btn-success">
-                    Weiter einkaufen
-                </Link>
-                <Link to="/" className="btn btn-primary">
-                    Zur Startseite
-                </Link>
-            </div>
-        </div>
-    );
+      <div className="thank-buttons">
+        <Link to="/shop" className="btn btn-orange">
+          Weiter einkaufen
+        </Link>
+        <Link to="/" className="btn btn-warning">
+          Zur Startseite
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ThankPage;
