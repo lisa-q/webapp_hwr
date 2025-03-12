55936a92adcd6e38ffe0c998b3874051a56c7c3e
laura194 <voss.2000@gmx.de>
2025-03-12 18:35:41 +0100

    move firebase realtime_database_key to .env

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -19,7 +19,7 @@ const serviceAccount = {
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+  databaseURL: process.env.FIREBAE_REALTIME_DATABASE_URL
 });
 
 export const db = admin.database();
